0408fa3faf4a1b1d9c8968e5e2743381ea1f7692
Igor Kulatov <igor.kulatov@gmail.com>
2019-08-14 23:06:58 +0200

    Json.obj sample

--- src/main/scala/es/ibs/example/RestService.scala
+++ src/main/scala/es/ibs/example/RestService.scala
@@ -8,11 +8,19 @@ import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import javax.inject.Inject
 import akka.http.scaladsl.model.StatusCodes._
+import play.api.libs.json.Json
+import es.ibs.util.json.AkkaPlayJsonSupport._
 
 class RestService @Inject() (SVC: RatesService)(implicit as: ActorSystem, fm: ActorMaterializer) {
 
   val route: Route =
     get {
+      path("k" / Segment) { code =>
+        complete {
+          if(code.length == 10 && code.startsWith("beg") && code.endsWith("fin")) Json.obj("sub_id" -> 218, "region" -> "GLOBAL", "price" -> 10.11)
+          else NotFound -> Json.obj()
+        }
+      } ~
       path(Segment / Segment / DoubleNumber) { case (from, to, amt) =>
         try {
           val res = SVC.convert(from.toUpperCase, to.toUpperCase, BigDecimal(amt))
